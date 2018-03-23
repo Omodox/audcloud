04b9387082987d345568d2a25190bcb5dc9465c1
Omodox <khoroshcho@gmail.com>
2018-03-23 18:34:52 +0200

    Commands

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+let rooms = [];
+
 io.on('connection', (socket) => {
   console.log('USER CONNECTED');
 
@@ -10,12 +12,28 @@ io.on('connection', (socket) => {
     console.log('USER DISCONNECTED');
   });
 
-  socket.on('add-message', (message) => {
-    io.emit('message', {type:'new-message', text: message});
-    console.log(message);
+  socket.on('add-message', (message,id) => {
+    console.log(id);
+    io.emit(id, {type:'new-message', text: message});
+    // console.log(message);
+    rooms.push(id);
+    console.log(rooms);
   });
+
+  socket.on('player', (message,id) => {
+    io.emit(id, {type:'command', text: message});
+   
+   
+  });
+
+});
+
+
+app.get("/about", function(request, response){
+     
+    response.send("<h1>О сайте</h1>");
 });
 
 http.listen(3000, () => {
-  console.log('started on port 8080');
+  console.log('started on port 3000');
 });
